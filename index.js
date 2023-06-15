94704170ab85d4c5bb290930ca75d6025707064e
krystal <krystal.mark@student.moringaschool.com>
2023-06-16 01:28:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(10); 
+
+  function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
